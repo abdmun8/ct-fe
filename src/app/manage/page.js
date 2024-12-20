5c8990e8c6af14cfb32d157852a852687433a3f3
Abdul Mun-im <abdul.munim@mst.co.id>
2024-12-20 08:41:25 +0700

    fix: build error useSearchParams

--- src/app/manage/page.js
+++ src/app/manage/page.js
@@ -1,9 +1,12 @@
 import FormTask from "@/components/form-task";
+import { Suspense } from "react";
 
 export default function Manage() {
   return (
     <div className="flex justify-center font-[family-name:var(--font-geist-sans)]">
-      <FormTask />
+      <Suspense>
+        <FormTask />
+      </Suspense>
     </div>
   );
 }
